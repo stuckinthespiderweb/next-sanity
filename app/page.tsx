e7af59f5929a0de532cf52bc277f36fc85e1dc14
stuckinthespiderweb <tim.destexhe@icloud.com>
2024-03-27 18:20:41 +0100

    &apos

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   return (
   
   <div className="max-w-5xl mx-auto py-20">
-    <h1 className="text-7xl font-extrabold">Hello I'm <span className="bg-gradient-to-r from-orange-400 via-purple-600 bg-clip-text text-transparent">Usermind</span></h1>
+    <h1 className="text-7xl font-extrabold">Hello I am <span className="bg-gradient-to-r from-orange-400 via-purple-600 bg-clip-text text-transparent">Usermind</span></h1>
     <p className="mt-3 text-xl text-gray-600">Check out my projects!</p>
     <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
 
